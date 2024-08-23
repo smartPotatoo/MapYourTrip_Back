f99760c86196bd6d48766baa56dfaa5117025a37
민석 <47300870+seok019283501@users.noreply.github.com>
2024-08-23 16:57:28 +0900

    feat: security 전체 허용

---
+++ src/main/java/com/smartpotatoo/map_your_trip_be/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.smartpotatoo.map_your_trip_be.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig{
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests((auth)->{
+                    auth.requestMatchers("*").permitAll()
+                            .anyRequest().authenticated();
+                });
+        httpSecurity.csrf((auth)->auth.disable());
+        httpSecurity.formLogin((auth)->{
+            auth.disable();
+        });
+        httpSecurity.httpBasic((auth)->{
+            auth.disable();
+        });
+
+        return httpSecurity.build();
+    }
+
+}
