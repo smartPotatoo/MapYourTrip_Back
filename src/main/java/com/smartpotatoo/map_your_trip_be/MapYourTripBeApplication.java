78c34d15531d507e07c6c90a108f0da30ef6f2ad
민석 <47300870+seok019283501@users.noreply.github.com>
2024-08-23 16:52:46 +0900

    first commit

---
+++ src/main/java/com/smartpotatoo/map_your_trip_be/MapYourTripBeApplication.java
@@ -0,0 +1,13 @@
+package com.smartpotatoo.map_your_trip_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapYourTripBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapYourTripBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartpotatoo/map_your_trip_be/MapYourTripBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartpotatoo.map_your_trip_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapYourTripBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
