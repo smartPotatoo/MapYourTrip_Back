5a0ec2d4d748109ef1b6571e4b178794ad0208c7
민석 <47300870+seok019283501@users.noreply.github.com>
2024-08-24 00:18:49 +0900

    password 암호화

--- src/main/java/com/smartpotatoo/map_your_trip_be/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/smartpotatoo/map_your_trip_be/domain/user/service/UserServiceImpl.java
@@ -5,17 +5,19 @@ import com.smartpotatoo.map_your_trip_be.domain.user.mapper.UserMapper;
 import com.smartpotatoo.map_your_trip_be.entity.user.UserRepository;
 import com.smartpotatoo.map_your_trip_be.entity.user.UsersEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     //회원가입
     public void join(JoinRequest joinRequest){
-        //회원가입;
         UsersEntity entity = UserMapper.toEntity(joinRequest);
+        entity.setPassword(bCryptPasswordEncoder.encode(joinRequest.getPassword()));
         userRepository.save(entity);
     }
 
