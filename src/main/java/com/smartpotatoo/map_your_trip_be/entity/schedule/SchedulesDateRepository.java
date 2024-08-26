8919dac46a8adcc9952c49aa340f96f869f6c653
Daviswhistle <46082741+Daviswhistle@users.noreply.github.com>
2024-08-26 09:22:44 +0900

    repository 생성

---
+++ src/main/java/com/smartpotatoo/map_your_trip_be/entity/schedule/SchedulesDateRepository.java
@@ -0,0 +1,7 @@
+package com.smartpotatoo.map_your_trip_be.entity.schedule;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchedulesDateRepository extends JpaRepository<SchedulesDateEntity, Long> {
+
+}
