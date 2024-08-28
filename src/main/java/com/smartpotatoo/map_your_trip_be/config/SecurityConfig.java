1f066de77575fbd6c40e7801a0b96957542127f5
민석 <47300870+seok019283501@users.noreply.github.com>
2024-08-28 19:52:31 +0900

    cors 수정

--- src/main/java/com/smartpotatoo/map_your_trip_be/config/SecurityConfig.java
+++ src/main/java/com/smartpotatoo/map_your_trip_be/config/SecurityConfig.java
@@ -4,12 +4,22 @@ import com.smartpotatoo.map_your_trip_be.filter.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -23,13 +33,10 @@ public class SecurityConfig{
                     auth.requestMatchers("/**").permitAll()
                             .anyRequest().authenticated();
                 });
-        httpSecurity.csrf((auth)->auth.disable());
-        httpSecurity.formLogin((auth)->{
-            auth.disable();
-        });
-        httpSecurity.httpBasic((auth)->{
-            auth.disable();
-        });
+        httpSecurity.cors(withDefaults());
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.formLogin(AbstractHttpConfigurer::disable);
+        httpSecurity.httpBasic(AbstractHttpConfigurer::disable);
         httpSecurity.sessionManagement((auth)->{
             auth.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         });
@@ -41,5 +48,17 @@ public class SecurityConfig{
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
