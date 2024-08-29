09e5f93e1340f9ebd666d9d4e9cbdb8f40c4582d
민석 <47300870+seok019283501@users.noreply.github.com>
2024-08-29 11:25:16 +0900

    cors 전체허용

--- src/main/java/com/smartpotatoo/map_your_trip_be/config/SecurityConfig.java
+++ src/main/java/com/smartpotatoo/map_your_trip_be/config/SecurityConfig.java
@@ -54,10 +54,9 @@ public class SecurityConfig{
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
-        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
