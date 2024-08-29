ea89dbf86e964091a1353e0f4f2093307ac57c8e
Daviswhistle <46082741+Daviswhistle@users.noreply.github.com>
2024-08-29 10:32:42 +0900

    cors 설정 변경

--- src/main/java/com/smartpotatoo/map_your_trip_be/config/WebMvcConfig.java
+++ src/main/java/com/smartpotatoo/map_your_trip_be/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
